8246e5c042cd2b13542d54bec29c730f2108d65e
srvinii <ericvinicius@linuxmail.org>
2016-06-11 12:17:45 -0300

    src files

---
+++ src/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define USER	"root"	// usuario padrão
+#define PORT	22	// porta padrão
+
+int main(int argc, char *argv[])
+{
+	printf("'-'\n");
+	return 0;
+}
